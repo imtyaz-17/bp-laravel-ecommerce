48be23f08dae42c1150021ade8e5438f5ad1c804
imtyaz <imtyazahmed70@gmail.com>
2025-09-20 21:22:46 +0600

    add migrations for categories, subcategories, products, and images

---
+++ database/migrations/2025_09_20_145031_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('subcategory_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique()->index();
+            $table->text('description')->nullable();
+            $table->decimal('old_price', 10, 2)->nullable()->index();
+            $table->decimal('new_price', 10, 2)->index();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

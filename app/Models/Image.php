92ea4fa2993c99d5a1006d30b76ee4c9a0208955
imtyaz <imtyazahmed70@gmail.com>
2025-09-20 21:39:25 +0600

    add polymorphic relationship to Image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Image extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'imageable_id',
+        'imageable_type',
+        'path'
+    ];
+
+    public function imageable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
